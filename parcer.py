a48cbc85aa80ccc0bdf8e4bfa47c4d434be7956d
Aleksa <72038770+AlesyaRabushka@users.noreply.github.com>
2023-02-06 21:24:11 +0300

    created the list of lists with info for every word + created list with bases of each word + create class Parser and structured into methods

--- parcer.py
+++ parcer.py
@@ -6,6 +6,7 @@ from nltk.stem.snowball import RussianStemmer
 
 import nltk
 import spacy
+import pymorphy2
 
 
 nltk.download('punkt')
@@ -15,67 +16,161 @@ nltk.download('wordnet')
 # nltk.download('averaged_perceptron_tagger_ru')
 
 # init pdf file
-file = open("Documents/example.pdf", 'rb')
+# file = open("Documents/example.pdf", 'rb')
 
 # read from file
-reader = PdfReader(file)
-print(len(reader.pages))
-page = reader.pages[0]
-text = page.extract_text() # get text from file
-# print(text)
-# print(sent_tokenize(text))
-# print(word_tokenize(text))
+# reader = PdfReader(file)
+# print(len(reader.pages))
+# page = reader.pages[0]
+# text = page.extract_text() # get text from file
+
 
 
 # get rid of necessary words
-stop_words = set(stopwords.words("russian"))
-filtered_list = []
-for word in word_tokenize(text):
-    if word.casefold() not in stop_words:
-        filtered_list.append(word)
+# stop_words = set(stopwords.words("russian"))
+# filtered_list = []
+# for word in word_tokenize(text):
+#     if word.casefold() not in stop_words:
+#         filtered_list.append(word)
 # print(filtered_list)
 
 # steamer = PorterStemmer()
 
 
 # get rid of punctuation symbols
-filtered = list(filter(lambda x: x != ',' and x != '.' and x != ', ', filtered_list))
-filtered = list(filtered)
+# filtered = list(filter(lambda x: x != ',' and x != '.' and x != ', ', filtered_list))
+# filtered = list(filtered)
 # print(f)
 
-import pymorphy2
-morph = pymorphy2.MorphAnalyzer()
-words_dict = {}
-normal_form_list =[]
 
-for token in filtered:
-    if 'ЗПР' not in morph.parse(token)[0].tag.cyr_repr and 'НЕИЗВ' not in morph.parse(token)[0].tag.cyr_repr and 'Н' not in morph.parse(token)[0].tag.cyr_repr and 'ЧАСТ' not in morph.parse(token)[0].tag.cyr_repr and '-' not in morph.parse(token)[0].word:
-        # print(morph.parse(token)[0].word ,morph.parse(token)[0].tag.cyr_repr) # get word and it's morph discription
-        words_dict[morph.parse(token)[0].word] = morph.parse(token)[0].tag.cyr_repr
-        # nf_list.append(morph.parse(token)[0].inflect({'sing', 'nomn'}))
-        # print(morph.parse(token)[0].normal_form)
-        normal_form_list.append(morph.parse(token)[0].normal_form)
-        # print('- ',morph.parse(token)[0].make_agree_with_number(1).word)
+# morph = pymorphy2.MorphAnalyzer()
+# words_dict = {}
+# normal_form_list =[]
+# word_info_list = []
+#
+# rs = RussianStemmer()
+# word_base_list = []
+
+# k = ['мама','Папа','арбуз','Яхта','олень']
+# k = sorted([x.lower() for x in k])
+# print(k)
+
+# b = []
+# for word in filtered:
+#     b.append(word.lower())
+#
+# filtered = sorted(b)
+# print(filtered)
 
 
-# sorted(words_dict.keys())
-# sorted_words_dict = sorted(words_dict)
-print(sorted(words_dict.items()))
-print(sorted(normal_form_list))
-for x in sorted(normal_form_list):
-    try:
-        for i in morph.parse(x)[0].lexeme:
-            print('-', i.word, '-', i.tag.cyr_repr)
-    except:
-        print(morph.parse(x)[0].word)
+#
+# for token in filtered:
+#     if 'ЗПР' not in morph.parse(token)[0].tag.cyr_repr and 'НЕИЗВ' not in morph.parse(token)[0].tag.cyr_repr and 'Н' not in morph.parse(token)[0].tag.cyr_repr and 'ЧАСТ' not in morph.parse(token)[0].tag.cyr_repr and '-' not in morph.parse(token)[0].word:
+#         # print(morph.parse(token)[0].word ,morph.parse(token)[0].tag.cyr_repr) # get word and it's morph discription
+#         # print(morph.parse(token))
+#         words_dict[morph.parse(token)[0].word] = morph.parse(token)[0].tag.cyr_repr.replace(',',' ').split()
+#         # nf_list.append(morph.parse(token)[0].inflect({'sing', 'nomn'}))
+#         # print(morph.parse(token)[0].normal_form)
+#         normal_form_list.append(morph.parse(token)[0].normal_form)
+#         # add to the info list a list of each word info
+#         word_info_list.append(morph.parse(token)[0].tag.cyr_repr.replace(',',' ').split())
+#         # get base of the word
+#         word_base_list.append(rs.stem(morph.parse(token)[0].word))
+#
+#
+#
+# # отсортированный словарь слов с доп инфой
+# print((words_dict.items()))
+# # отсортированный список начальных форм слова
+# print((normal_form_list))
+# print(word_info_list)
+# print(word_base_list)
+
+
+# for x in sorted(normal_form_list):
+#     try:
+#         for i in morph.parse(x)[0].lexeme: # склонение слов по падежам
+#             print('-', i.word, '-', i.tag.cyr_repr)
+#     except:
+#         print(morph.parse(x)[0].word)
     # print('3 - ', morph.parse(x)[0].make_agree_with_number(3).word)
     # print('4 - ', morph.parse(x)[0].make_agree_with_number(5).word)
 
 
 
 
-# rs = RussianStemmer()
-# print(rs.stem('полиграфы'))
+
+
+
+
+
+
+class Parser:
+    def __init__(self, filename):
+        # init pdf file   "Documents/example.pdf"
+        self.file = open(filename, 'rb')
+
+        # read from file
+        self.reader = PdfReader(self.file)
+        # print(len(reader.pages))
+        self.page = self.reader.pages[0]
+        self.text = self.page.extract_text()  # get text from file
+
+        self.morph = pymorphy2.MorphAnalyzer()
+        self.stemmer = RussianStemmer()
+
+
+        # all needed lists
+        self.words_dict = {}
+        self.filtered_list = []
+        self.normal_form_list = []
+        self.word_info_list = []
+        self.word_base_list = []
+
+
+
+    def filter_text(self):
+        # get rid of necessary words
+        stop_words = set(stopwords.words("russian"))
+        for word in word_tokenize(self.text):
+            if word.casefold() not in stop_words:
+                self.filtered_list.append(word)
+
+        # get rid of punctuation symbols
+        filtered = list(filter(lambda x: x != ',' and x != '.' and x != ', ', self.filtered_list))
+        self.filtered_list = list(filtered)
+
+        # lowcase all the words and sort them
+        self.filtered_list = sorted([x.lower() for x in self.filtered_list])
+
+
+
+    def get_word_info(self):
+        for token in self.filtered_list:
+            if 'ЗПР' not in self.morph.parse(token)[0].tag.cyr_repr and 'НЕИЗВ' not in self.morph.parse(token)[0].tag.cyr_repr and 'Н' not in self.morph.parse(token)[0].tag.cyr_repr and 'ЧАСТ' not in self.morph.parse(token)[0].tag.cyr_repr and '-' not in self.morph.parse(token)[0].word:
+                # print(morph.parse(token)[0].word ,morph.parse(token)[0].tag.cyr_repr) # get word and it's morph discription
+                # print(morph.parse(token))
+                self.words_dict[self.morph.parse(token)[0].word] = self.morph.parse(token)[0].tag.cyr_repr.replace(',',' ').split()
+                # get word's NORMAL FORM
+                self.normal_form_list.append(self.morph.parse(token)[0].normal_form)
+                # add to the info list a list of each word info
+                self.word_info_list.append(self.morph.parse(token)[0].tag.cyr_repr.replace(',',' ').split())
+                # get base of the word
+                self.word_base_list.append(self.stemmer.stem(self.morph.parse(token)[0].word))
+
+    def show_info(self):
+        print('dict ', self.words_dict)
+        print('normal form ', self.normal_form_list)
+        print('info ', self.word_info_list)
+        print('base ', self.word_base_list)
+
+
+if __name__ == '__main__':
+    parser = Parser("Documents/example.pdf")
+    parser.filter_text()
+    parser.get_word_info()
+    parser.show_info()
+
 # s = [rs.stem(word) for word in f]
 # print(list(s))
 #
@@ -125,33 +220,4 @@ for x in sorted(normal_form_list):
 
 
 
-
-class Parcer:
-    def __init__(self, file):
-        self.file = ''
-        self.reader = PdfReader(file)
-        self.text = ''
-        self.word_tokenize = []
-        self.sent_tokenize = []
-
-    # get text from the pdf file
-    def read_text_from_pdf(self, file):
-        self.file = file
-        page = self.reader.pages[0]
-        self.text = page.extract_text()
-        self.sent_tokenize = sent_tokenize(self.text)
-        self.word_tokenize = word_tokenize(self.text)
-
-    # filter stopwords out of the text
-    def filter_text(self):
-        stop_words = set(stopwords.words("russian"))
-        filtered_list = []
-        for word in self.word_tokenize:
-            if word.casefold() not in stop_words:
-                filtered_list.append(word)
-        print(filtered_list)
-        return filtered_list
-
-    def stemm_text(self):
-        pass
 
